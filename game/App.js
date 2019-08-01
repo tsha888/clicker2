ec9cd3a90820d1ae51862a41d464c790f160114a
michael joh <michaeljoh99@gmail.com>
2019-08-01 19:34:35 -0400

    clicker draft completed

--- game/App.js
+++ game/App.js
@@ -1,19 +1,83 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Button } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      number: 0
+    };
+  }
+
+  clicker() {
+    this.setState({
+      number: this.state.number + 1
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.clickerContainer}
+          onPress={this.clicker.bind(this)}
+        >
+          <Text style={styles.clickerCount}> {this.state.number} Cookies</Text>
+        </TouchableOpacity>
+        <View style={styles.shopContainer}>
+          <TouchableOpacity style={styles.bottomIcons}
+            // onPress={this.clicker.bind(this)}
+          >
+            <Text>The Shop</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  clickerContainer: {
+    flex: 9 / 10,
+    backgroundColor: "pink",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  clickerCount: {
+    fontSize: 40
+  },
+  shopContainer: {
+    flex: 1 / 10,
+    backgroundColor: "blue",
+    flexDirection: "row",
+  },
+  bottomIcons: {
+    backgroundColor: "white", 
+    flex: 1 / 3,
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
+
+// function game(props) {
+//   const [counter, setCounter] = useState(0)
+
+//   function increaseNumber(e) {
+//     counter = this.state.counter + 1;
+//   }
+
+//   return (
+//   <View style={styles.container}>
+//   <TouchableOpacity
+//     style={styles.clickerContainer}
+//     onPress={e => increaseNumber(e)}
+//   >
+//     <Text> Clicker: {this.state.counter} </Text>
+//   </TouchableOpacity>
+//   <View style={styles.shopContainer} />
+// </View>
+// );
+// }
